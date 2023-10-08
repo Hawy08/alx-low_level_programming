be8db8703b6aa5c15c34f0c8d575011196f8a950
Hawy08 <ridanhawy@gmail.com>
2023-10-08 13:38:46 -0700

    check

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+/**
+ * main-entry point
+ * Return:Always zero
+ * */
 
 int main(void)
 {
 	int n = 0;
 	char x = 'a';
 
-	for(n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 		putchar('0' + n);
-	while(x <= 'f')
+	while (x <= 'f')
 	{
 		putchar(x);
 		x++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
