b0605500656f631afa3b850ca6a2bbc5b3ebeea1
Hawy08 <ridanhawy@gmail.com>
2023-10-08 13:27:07 -0700

    check

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/**
+ * main-print
+ * Return:always zero
+ */
 
 int main(void)
 {
 	int num = 0;
-	for(num = 0; num < 10; num++)
-		printf("%d",num);
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 	printf("\n");
-	return(0);
+	return (0);
 }
