dfe921544e899fa4a510f0b0de81c3a8341dfae0
Ridan Hawi <ridanhawy@gmail.com>
2023-10-17 03:30:32 +0300

    First commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -12,10 +13,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		if (i == (n - 1))
-			break;
-		printf(", ");
+	_putchar("%d", a[i]);
+	if (i == (n - 1))
+		break;
+	_putchar(", ");
 	}
 	_putchar('\n');
 }
