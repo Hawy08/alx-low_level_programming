6e707f5bf4fdebaeb5b413a325ceb86272ad6192
Ridan Hawi <ridanhawy@gmail.com>
2023-10-28 00:07:13 +0300

    First commit

---
+++ 0x09-static_libraries/0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main funtion
+ * @argc: count of arguments supplied in cli
+ * @argv: array of strings which are those arguments
+ * Return: always 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+        int num1 = 0, num2 = 0;
+
+        if (argc == 3)
+        {
+                num1 = atoi(argv[1]);
+                num2 = atoi(argv[2]);
+                printf("%d\n", num1 * num2);
+                return (0);
+        }
+        else
+        {
+                printf("Error\n");
+                return (1);
+        }
+}
