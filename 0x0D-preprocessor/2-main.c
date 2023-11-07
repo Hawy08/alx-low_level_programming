cc748423a5dedb792273f7c76e22212ad0a164b6
Ridan Hawi <ridanhawy@gmail.com>
2023-11-07 04:14:48 +0300

    Changes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
 * printFileName-Prints the name of the file it was compiled from.
+* Return: 0
 */
-void printFileName(void)
+int printFileName(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
